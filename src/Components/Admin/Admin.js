08960d44eb6ed4b9c61f4024dfe42dda727f14c7
tanimpersonal <tashintanim2019@gmail.com>
2022-08-03 22:50:24 +0600

    image-upload

--- src/Components/Admin/Admin.js
+++ src/Components/Admin/Admin.js
@@ -1,21 +1,39 @@
-import React, { useState } from "react";
+import axios from "axios";
+import React, { useRef, useState } from "react";
 
 const Admin = () => {
   const [image, setImage] = useState([]);
-  const handleChange = () => {};
-  const handleSubmit = () => {};
+
+  const imageInput = useRef("");
+  const handleChange = () => {
+    // console.log(imageInput.current.files[0]);
+    const image = imageInput.current.files[0];
+    setImage(image);
+  };
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+    const formData = new FormData();
+    formData.append("image", image);
+    await axios
+      .post(
+        "https://api.imgbb.com/1/upload?key=e8b0dad22118468cdad63c218aef6c48",
+        formData
+      )
+      .then(async (res) => {});
+  };
   return (
     <div>
       <h1>Add Picture</h1>
-      <form>
+      <form onSubmit={handleSubmit}>
         <input
           type="file"
           accept=".pnj, .jpeg, .jpg"
           onChange={handleChange}
           name="image"
           id=""
+          ref={imageInput}
         />
-        <input type="submit" onSubmit={handleSubmit} value="Upload Picture" />
+        <input type="submit" value="Upload Picture" />
       </form>
     </div>
   );
